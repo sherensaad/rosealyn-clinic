9b08b456345807308e54c328e767aa5bccc40068
sherensaad <108844662+sherensaad@users.noreply.github.com>
2026-05-10 11:51:00 +0300

    fix db ssl

--- db.js
+++ db.js
@@ -3,12 +3,15 @@ require("dotenv").config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  },
   waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  connectionLimit: 10
 });
 
 module.exports = pool;
